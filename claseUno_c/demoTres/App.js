59d6b71631a77342ad9404559c070d46a6bf7440
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2025-10-13 17:33:59 -0300

    demo tres

---
+++ claseUno_c/demoTres/App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Probando imagenes</Text>
+      <Image 
+      source = {require('./assets/zonaMedia.jpeg')}
+      resizeMode='contain'/>
+      <Image style = {styles.image}
+      source={{uri:'https://www.infobae.com/resizer/v2/CDTA65FYWMDLJV6AHKPIIPNOHU.jpg?auth=6c9f6791e78e189e11a8ff0474b608b95906885697189234ddcdee212e19f2bd&smart=true&width=992&height=661&quality=85'}}
+      resizeMode='contain' />
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+  width: '200%',
+  height: 200, 
+},
+});
